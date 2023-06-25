e0707d18f25c295f9d177ca4be54e870ec8d3c13
Serhii Savruk <serhii.savruk@pm.me>
2023-06-25 21:22:24 +0300

    Add 217 third option

--- 217-contains-duplicate/task.go
+++ 217-contains-duplicate/task.go
@@ -1,18 +1,10 @@
 package main
 
 func containsDuplicate(nums []int) bool {
-	hasZero := false
 	m := make(map[int]int)
 	for _, num := range nums {
-		if num == 0 {
-			if hasZero {
-				return true
-			}
-			hasZero = true
-			continue
-		}
-
-		if m[num] == num {
+		_, ok := m[num]
+		if ok {
 			return true
 		}
 		m[num] = num
